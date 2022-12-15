516c54412bf7c327d000330b12a46d017ba32411
Eric Chen <ericchen@Erics-MacBook-Pro.local>
2022-12-14 22:19:22 -0500

    users

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 import express from 'express';
 import cors from 'cors';
+import session from 'express-session';
 import mongoose from "mongoose";
 import PostingsController from "./controllers/postings/postings-controller.js";
-import UserController from "./controllers/users/user-controller.js";
+import UserController from "./controllers/users/users-controller.js";
 
-const CONNECTION_STRING = 'mongodb+srv://cs4550:project@postings.u9fsees.mongodb.net/postings?retryWrites=true&w=majority'
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
+    maxPoolSize: 10,
+    socketTimeoutMS: 45000,
+    family: 4
+}
+
+const CONNECTION_STRING = 'mongodb+srv://cs4550:project@postings.u9fsees.mongodb.net/?retryWrites=true&w=majority'
 console.log(CONNECTION_STRING)
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, options);
 
-const app = express()
-app.use(cors())
+const app = express();
+app.use(cors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+}))
+app.use(session({
+    secret: 'should be an environment variable',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
 app.use(express.json())
 PostingsController(app)
 UserController(app)
-app.listen(process.env.PORT || 4000)
+app.listen(4000)
